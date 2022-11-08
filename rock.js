c0644f5fa8f1268b0feb4ca6449c03af712c952f
coffee-marios <mark_theo@yahoo.com>
2022-11-08 09:58:53 +0200

    Set the structure of the game
    We prepared the main functions that we are going to need for this stage. The results will appear only on the console.

---
+++ rock.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function getComputerChoice(){
+    // randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+}
+
+
+function singleRound(playerSelection, computerSelection){
+
+}
+
+function game(){
+
+}
+
+
+console.log(true);
