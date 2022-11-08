42cb31bc59bba8ec46a2b16882737f43f0f8f4cf
coffee-marios <mark_theo@yahoo.com>
2022-11-08 11:34:29 +0200

    Complete function for a single round

--- rock.js
+++ rock.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function getComputerChoice(){
-    // randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+    // randomly return either ‘Rock’, ‘Paper’ or ‘scissorss’
     let computerSelection;
 
     let randomNumber;
@@ -15,7 +15,7 @@ function getComputerChoice(){
             computerSelection = "rock";
             break;
         case 2:
-            computerSelection = "scissor";
+            computerSelection = "scissors";
             
     }
 
@@ -23,12 +23,58 @@ function getComputerChoice(){
 
 }
 
-console.log(getComputerChoice());
 
 
 function singleRound(playerSelection, computerSelection){
+    
+    let result;
+    
+    if (playerSelection==="paper"){
+        switch (computerSelection){
+            case "paper":
+                result = "That's a draw. You both chose paper.";
+                break;
+            case "rock":
+                result = "You won. Paper covers rock";
+                break;
+            case "scissors":
+                result = "You lost. Scissors cuts paper.";
+        }}
 
-}
+    if (playerSelection==="rock"){
+        switch (computerSelection){
+            case "paper":
+                result = "You lost. Paper covers rock.";
+                break;
+            case "rock":
+                result = "That's a draw. You both chose rock.";
+                break;
+            case "scissors":
+                result = "You won. Rock crushes scissors.";
+        }}
+
+    if (playerSelection==="scissors"){
+        switch (computerSelection){
+            case "paper":
+                result = "You won. Scissors cuts paper.";
+                break;
+            case "rock":
+                result = "You won. Rock crushes scissors.";
+                break;
+            case "scissors":
+                result = "That's a draw. You both chose scissors.";
+        }}
+    
+        return result; 
+
+    }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(singleRound(playerSelection, computerSelection));
+
+
 
 function game(){
 
