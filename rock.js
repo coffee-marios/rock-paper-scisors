46d9bd45ddf2112b2b891a2e1da026f04d73b3c4
coffee-marios <mark_theo@yahoo.com>
2022-11-08 10:57:50 +0200

    Complete function for random computer selection

--- rock.js
+++ rock.js
@@ -2,7 +2,28 @@
 
 function getComputerChoice(){
     // randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+    let computerSelection;
+
+    let randomNumber;
+    randomNumber = Math.floor(Math.random() * 3);
+
+    switch (randomNumber){
+        case 0:
+            computerSelection = "paper";
+            break;
+        case 1:
+            computerSelection = "rock";
+            break;
+        case 2:
+            computerSelection = "scissor";
+            
+    }
+
+    return computerSelection;
+
 }
+
+console.log(getComputerChoice());
 
 
 function singleRound(playerSelection, computerSelection){
